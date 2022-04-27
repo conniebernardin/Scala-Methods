a9420e609dffdd0e0bbb14aba109e60336f36f80
Connie Bernardin <conniebernardin@googlemail.com>
2022-04-27 17:06:09 +0100

    tests for fizz buzz

--- src/main/scala/IntToString.scala
+++ src/main/scala/IntToString.scala
@@ -3,15 +3,18 @@ class IntToString {
   val fizzBuzz = (a:Int)=> {
 
     // val fizz = "Fizz"
-    if(a % 3 == 0){
-      a.toString + "Fizz"
+    if(a % 3 == 0 && a%5 !=0 && a != 0){
+      a.toString + " Fizz"
   }
-    else if (a % 5 == 0){
-      a.toString + "Buzz"
+    else if (a % 5 == 0 && a%3 !=0 && a != 0){
+      a.toString + " Buzz"
   }
-    else if(a%3 == 0 && a%5 == 0){
-      a.toString + "FizzBuzz"
+    else if(a%3 == 0 & a%5 == 0 && a != 0){
+      a.toString + " FizzBuzz"
   }
+    else {
+      "no fizz or buzz for you"
+    }
   }
 
 }
--- src/test/scala/FizzBuzzSpec.scala
+++ src/test/scala/FizzBuzzSpec.scala
@@ -1,3 +1,30 @@
-class FizzBuzzSpec {
+import org.scalatest.FlatSpec
+
+class FizzBuzzSpec extends FlatSpec {
+
+  "fizzbuzz" should "modulus by 3 should fizz" in {
+    val nums = new IntToString
+    assert(nums.fizzBuzz(9) === "9 Fizz")
+  }
+
+  "fizzbuzz" should "modulus by 5 should buzz" in {
+    val nums = new IntToString
+    assert(nums.fizzBuzz(10) === "10 Buzz")
+  }
+
+  "fizzbuzz" should "modulus by 3 and 5 should FizzBuzz" in {
+    val nums = new IntToString
+    assert(nums.fizzBuzz(15) === "15 FizzBuzz")
+  }
+
+  "fizzbuzz" should "return sassy statement if not divisible by 3 or 5" in {
+    val nums = new IntToString
+    assert(nums.fizzBuzz(13) === "no fizz or buzz for you")
+  }
+
+  "fizzbuzz" should "return sassy statement if 0" in {
+    val nums = new IntToString
+    assert(nums.fizzBuzz(0) === "no fizz or buzz for you")
+  }
 
 }
